efb7431563e8526e12262c99fb43ce43975cb45b
Mounir <hamzaouimounirlfig@gmail.com>
2017-03-09 23:11:53 +0100

    Add Facebook Oauth link to Laravel API

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -20,7 +20,10 @@ export class SignInPage {
  userinfo={};
  error='';
  connected=false;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController,private API:ApiService,public aclService:AclService, public loader:LoadingController) {}
+ FB_APP_ID: number = 1368002846591785;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController,private API:ApiService,public aclService:AclService, public loader:LoadingController) {
+    Facebook.browserInit(this.FB_APP_ID);
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignInPage');
@@ -49,7 +52,60 @@ export class SignInPage {
 
      });
   }
+  facebookOath(){
+    //check if the user has already logged in
+       this.checkConnectivityState();
+       if(!this.connected){
+           //Connexion à Facebook
+           let permissions = new Array();
+           //the permissions your facebook app needs from the user
+           permissions = ["public_profile"];
+           Facebook.login(permissions)
+           .then((response)=>{
+             alert("**ACCESS TOKEN =**"+JSON.stringify(response.authResponse.accessToken));
+             let userId = response.authResponse.userID;
+             let accessToken=response.authResponse.accessToken;
+             let params = new Array();
+             //Getting user information
+             Facebook.api("/me?fields=name,gender,age_range,first_name,last_name", params)
+             .then((user) =>{
+               user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+               alert(JSON.stringify(user));
 
+                // let authParams={provider:'facebook',oauthUser:user}
+                 // alert("PROVIDER ID SENT TO API"+JSON.stringify(user));
+                 let callback=this.API.all('auth').one('facebook',accessToken);
+                 //alert(JSON.stringify(callback));
+                 callback.get().subscribe(
+                   (callbackResponse)=>{
+                     alert("success");
+                     alert(JSON.stringify(callbackResponse.data));
+                    // this.output=callbackResponse.data;
+                     /*if(!callbackResponse.errors){
+                         //alert('Laravel conn');
+                         alert(JSON.stringify(callbackResponse.data));
+
+                     }*/
+
+                   }
+                 );
+
+
+                 /*
+                 In case of need these code below will use a post method to send OauthProviderId and the provider to the api
+                 this.service.facebookAuthPOSTHttp(user.id).subscribe(
+                     (resp)=>{
+                         alert(JSON.stringify(resp));
+                     });
+                 */
+             })
+
+
+           }, (error)=>{
+             alert(error);
+           });
+       }
+  }
   /*logout(){
    this.connected=false;
    this.aclService.data.roles=[];
@@ -81,5 +137,67 @@ export class SignInPage {
  goToTermsAndConditions(){
    alert('Termes & Conditions page');
  }
+
+  checkStatusChangeCallback(){
+      alert('Checking the Status Changes');
+  }
+  disconnectFromFacebook(){
+    Facebook.logout().then((facebookDisconnectionResponse)=>{
+        alert('Disconnected User');
+        alert(facebookDisconnectionResponse);
+        this.connected=false;
+    },(error)=>{
+        alert(error);
+    })
+  }
+  checkConnectivityState(){
+    Facebook.getLoginStatus().then((response)=> {
+    this.statusChangeCallback(response);
+  });
+  }
+  // This is called with the results from from Facebook.getLoginStatus().
+   statusChangeCallback(response) {
+    alert('statusChangeCallback');
+    alert(JSON.stringify(response));
+
+
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for Facebook.getLoginStatus().
+    if (response.status === 'connected') {
+      this.connected=true;
+      // Logged into your app and Facebook.
+      alert('Already connected')
+    alert('Fetching your information.... ');
+    Facebook.api('/me',null).then((response)=> {
+      console.log('Successful login for: ' + response.name)
+      alert('Thanks for logging in, ' + response.name + '!')
+    })
+    alert('end of fetching ')
+
+     // this.testAPI(this.Facebook);
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+          alert('not_authorized');
+          this.connected=false;
+
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+          alert('not_authorized');
+          this.connected=false;
+
+    }
+  }
+   checkLoginState(Facebook) {
+
+      Facebook.getLoginStatus(
+        (response)=> {
+        this.statusChangeCallback(response)
+      }
+      );
+  }
+
 
 }
